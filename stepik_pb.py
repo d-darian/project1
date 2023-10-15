3d8080393443ca06656e7e14d7557f5611935a4f
Darian Domocos <dariandomocos59@gmail.com>
2023-10-15 17:38:35 +0300

    Initial Commit

---
+++ stepik_pb.py
@@ -0,0 +1,22 @@
+
+
+n =  int(input())
+r1 = []
+r2 = []
+for i in range(0, n*n):
+    x = int(input())
+    r1.append(x)
+
+
+for i in range(0, n*n):
+    x = int(input())
+    r1[i] += x
+
+
+for i in range(n):
+   r2.append(r1[i])
+   r1.pop(i)
+   i-=1
+
+print(r1)
+print(r2)
